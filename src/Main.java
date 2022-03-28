35c5d8bde094c0fab82843d333f9949094c712a2
phongteaa <phongteaa@gmail.com>
2022-03-28 09:13:02 +0700

    sua

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 // driver class
 public class Main {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
 
         MountainBike mb = new MountainBike(3, 100, 25);
         System.out.println("\nMountain bicycle ------>");
         System.out.println(mb.toString());
 
-        KidBicycle kb = new KidBicycle(2, 50, 3);
+        KidBicycle kb = new KidBicycle(1, 50, 3);
         System.out.println("\nKid bicycle =====>");
         System.out.println(kb.toString());
     }
